b0ce9be5fa7ce58b0e4ee6e1b932e0e129eb23e6
Bhavya Verma <me.bhavyaverma111@gmail.com>
2023-02-26 23:12:27 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-
+import 'react-toastify/dist/ReactToastify.css';
 import TaskManager from "./Component/TaskManagement";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
+    <ToastContainer/>
       <TaskManager />
     </div>
   );
